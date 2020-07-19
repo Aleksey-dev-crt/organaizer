4508d7cc07790bd95b8c8bb40399f9f8d33490f1
Aleksey-dev-crt <64360318+Aleksey-dev-crt@users.noreply.github.com>
2020-07-19 12:19:41 +0700

    add categories, tasks components

---
+++ src/app/views/categories/categories.component.ts
@@ -0,0 +1,26 @@
+import {Component, OnInit} from '@angular/core';
+import {DataHandlerService} from '../../service/data-handler.service';
+import {Category} from '../../model/Category';
+
+@Component({
+  selector: 'app-categories',
+  templateUrl: './categories.component.html',
+  styleUrls: ['./categories.component.css']
+})
+export class CategoriesComponent implements OnInit {
+
+  categories: Category[];
+
+  constructor(private dataHandlerService: DataHandlerService) {
+
+  }
+
+  ngOnInit(): void {
+    this.dataHandlerService.categoriesSubject.subscribe(categories => this.categories = categories);
+  }
+
+  showTasksByCategory(category: Category) {
+    this.dataHandlerService.fillTasksByCategory(category);
+  }
+
+}
